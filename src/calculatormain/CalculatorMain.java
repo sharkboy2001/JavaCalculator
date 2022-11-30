06560ef3982777cada283de2a72e3bb2e4ae46e5
Kemp <elliot.kemp@lyon.edu>
2022-11-30 10:45:49 -0600

    First commit

---
+++ src/calculatormain/CalculatorMain.java
@@ -0,0 +1,11 @@
+package calculatormain;
+
+public class CalculatorMain {
+    
+    public static void main(String[] args) {
+        Calc g = new Calc();
+        g.setSize(500, 525);
+        g.setVisible(true);
+    }
+    
+}
